910129f5552f91c566b7ad48a436755081dc1585
unknown <kajganicmarko@gmail.com>
2018-01-21 23:06:26 +0100

    zadatak4

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Posts } from '../../models/posts.model'
 import { PostService } from '../../services/post.service'
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-post-form',
@@ -12,7 +15,10 @@ export class PostFormComponent implements OnInit {
 
   _postService: PostService;
 
-  constructor(_postService: PostService) { 
+  constructor(_postService: PostService,
+              private router: Router,
+              private route: ActivatedRoute) {
+
     this._postService = _postService;
   }
 
@@ -22,6 +28,7 @@ export class PostFormComponent implements OnInit {
   submit(newPost)
   {
     this._postService.addPost(newPost);
+    this.router.navigate(['/posts']);
   }
 
 
